b0c04fc5086f9e7cc8265008ac1687d5665a45eb
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2023-01-17 00:04:07 -0800

    Update Hail_sample_QC.py

--- modules/Hail_sample_QC.py
+++ modules/Hail_sample_QC.py
@@ -6,11 +6,11 @@
 # In[91]:
 
 import sys
-temp_directory=sys.argv[2]
+#temp_directory=sys.argv[2]
 
 import hail as hl
 from hail.plot import output_notebook, show
-hl.init(tmp_dir=temp_directory)
+#hl.init(tmp_dir=temp_directory)
 output_notebook()
 
 
