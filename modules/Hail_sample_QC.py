94796e9f08f03416c0cad5db733e9f1931088aab
Solenne Correard <54953390+scorreard@users.noreply.github.com>
2023-01-18 17:12:26 -0800

    Update Hail_sample_QC.py

--- modules/Hail_sample_QC.py
+++ modules/Hail_sample_QC.py
@@ -60,9 +60,7 @@ def stat(table):
     Low_threashold = Mean - 3*StdDev
     High_threashold = Mean + 3*StdDev
     min_graph = table[table.columns[1]]. min() - 3*StdDev
-    if(np.isnan(min_graph)):  min_graph = 0
     max_graph = table[table.columns[1]]. max() + 3*StdDev
-    if(np.isnan(max_graph)):  max_graph = 0
     return Mean, StdDev, Low_threashold, High_threashold, min_graph, max_graph
 
 
